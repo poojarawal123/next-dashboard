40ffc6ee298ff40447a49b4e91f4c1a158058fa7
Pooja Rawal <pooja.rawal@brainvire.com@BVIAHMLAP-470.brainvire.com>
2024-11-06 11:22:37 +0530

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
@@ -28,6 +31,15 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div className={styles.shape} />
+      <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
     </main>
+    
   );
 }
