3838e9eca25d9d8392d9f82d85bb459a502ea143
Pooja Rawal <pooja.rawal@brainvire.com@BVIAHMLAP-470.brainvire.com>
2024-11-06 11:45:02 +0530

    Removed Error

--- nextjs-image-gallery/components/Carousel.tsx
+++ nextjs-image-gallery/components/Carousel.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import useKeypress from "react-use-keypress";
 import type { ImageProps } from "../utils/types";
 import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
 import SharedModal from "./SharedModal";
@@ -23,10 +22,6 @@ export default function Carousel({
   function changePhotoId(newVal: number) {
     return newVal;
   }
-
-  useKeypress("Escape", () => {
-    closeModal();
-  });
 
   return (
     <div className="fixed inset-0 flex items-center justify-center">
